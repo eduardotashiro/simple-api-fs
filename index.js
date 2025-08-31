5345ac4a24ebe8b9d3bd9d552902f821746aa6ac
Eduardo Tashiro <eduardogaia854@hotmail.com>
2025-08-30 23:32:24 -0300

    feat: adiciona rota /api/product?id=... e tratamento de produto não encontrado

--- index.js
+++ index.js
@@ -7,18 +7,34 @@ const data = readFileSync('./data/products.json', 'utf-8')
 const products = JSON.parse(data)
 
 const server = http.createServer((req, res) => {
-    const pathName = req.url
-    if (pathName === '/' || pathName === '/home') {
+    const { pathname, query } = url.parse(req.url, true)
+
+    if (pathname === '/' || pathname === '/home') {
         res.writeHead(200, { 'content-type': 'text/plain' })
         res.end('Welcome to the products API')
-    } else if (pathName === '/api/products') {
+
+    } else if (pathname === '/api/products') {
         res.writeHead(200, { 'content-type': 'application/json' })
         res.end(JSON.stringify(products))
+
+    } else if (pathname === '/api/product') {
+        const product = products.find(p => p.id == query.id)
+
+        if (product) {
+            res.writeHead(200, { 'content-type': 'application/json' })
+            res.end(JSON.stringify(product))
+        } else {
+            res.writeHead(404, { 'content-type': 'application/json' });
+            res.end(JSON.stringify({ message: 'Produto não encontrado' }));
+        }
     } else {
         res.writeHead(404, { 'content-type': 'text/html' })
         res.end('<h1>Page Not Found, sorry sorry</h1>')
     }
 })
+
+
+
 
 server.listen(8000, '127.0.0.1', () => {
     console.log("lISTENING TO REQ ON PORT 8000")
