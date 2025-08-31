9ca0c972dd92e96a8b9b3840f3476cd119a6e182
Eduardo Tashiro <eduardogaia854@hotmail.com>
2025-08-30 21:59:40 -0300

    feat: adiciona rotas home e 404

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
-import { readFileSync } from 'fs';
+import { readFileSync } from 'fs'
 import http from 'http'
+import url from 'url'
 
 
-const data = readFileSync("./data/products.json")
-const dataObject = JSON.parse(data)
+const data = readFileSync('./data/products.json', 'utf-8')
+const product = JSON.parse(data)
 
 const server = http.createServer((req, res) =>{
-    res.end( "Server ON")
+    const pathName = req.url
+   if (pathName === '/' || pathName === '/home') {
+        res.writeHead(200,{ 'content-type': 'text/plain'})
+        res.end(' Welcome to the products API ')
+   } else {
+    res.writeHead(404, { 'content-type': 'text/html'})
+    res.end('<h1>Page Not Found, sorry sorry</h1>')
+   }
 })
 
-server.listen(8000,"127.0.0.1", () =>{
+server.listen(8000,'127.0.0.1', () =>{
     console.log("lISTENING TO REQ ON PORT 8000")
 })
