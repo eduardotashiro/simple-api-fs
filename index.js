f023f57a30622978955b67646b1eebd0d7611c7b
Eduardo Tashiro <eduardogaia854@hotmail.com>
2025-08-30 22:38:39 -0300

    feat: adiciona rota /api/products retornando JSON

--- index.js
+++ index.js
@@ -4,19 +4,22 @@ import url from 'url'
 
 
 const data = readFileSync('./data/products.json', 'utf-8')
-const product = JSON.parse(data)
+const products = JSON.parse(data)
 
-const server = http.createServer((req, res) =>{
+const server = http.createServer((req, res) => {
     const pathName = req.url
-   if (pathName === '/' || pathName === '/home') {
-        res.writeHead(200,{ 'content-type': 'text/plain'})
-        res.end(' Welcome to the products API ')
-   } else {
-    res.writeHead(404, { 'content-type': 'text/html'})
-    res.end('<h1>Page Not Found, sorry sorry</h1>')
-   }
+    if (pathName === '/' || pathName === '/home') {
+        res.writeHead(200, { 'content-type': 'text/plain' })
+        res.end('Welcome to the products API')
+    } else if (pathName === '/api/products') {
+        res.writeHead(200, { 'content-type': 'application/json' })
+        res.end(JSON.stringify(products))
+    } else {
+        res.writeHead(404, { 'content-type': 'text/html' })
+        res.end('<h1>Page Not Found, sorry sorry</h1>')
+    }
 })
 
-server.listen(8000,'127.0.0.1', () =>{
+server.listen(8000, '127.0.0.1', () => {
     console.log("lISTENING TO REQ ON PORT 8000")
 })
